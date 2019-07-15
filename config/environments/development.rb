ab7ecd0542e6136d4ef8982f9bda0a1555f6d87c
Icaro Rezende <icaronr@gmail.com>
2019-07-15 11:55:59 -0300

    Correçao na porta do Mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,6 +55,6 @@ Rails.application.configure do
   # mailer options
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => 'mailcatcher', :port => 1025 }
+  config.action_mailer.smtp_settings = { :address => 'mailcatcher', :port => 25 }
 
 end
