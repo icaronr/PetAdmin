9ad43aafec883c76a08857a174e73a5d1d17e237
Icaro Rezende <icaronr@gmail.com>
2019-07-15 21:55:29 -0300

    Atualizando a Campanha

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,10 +1,6 @@
 class Campaign < ApplicationRecord
-  # fae stuff
   include Fae::BaseModelConcern
-  def fae_display_field
-    title
-  end
-
+  
   # relations
   has_many :campaign_clients, dependent: :destroy
   has_many :clients, through: :campaign_clients
@@ -12,4 +8,17 @@ class Campaign < ApplicationRecord
   # validations
   validates :title, presence: true
   validates :body, presence: true
+
+  after_save :schedule_emails
+
+  def schedule_emails
+    Client.all.each do |client|
+      CampaignClient.create(campaign: self, client: client)
+      CampaingJob.perform_later client, self.title, self.body
+    end
+  end
+  
+  def fae_display_field
+    title
+  end
 end
